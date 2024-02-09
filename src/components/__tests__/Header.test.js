4c1804c11896551407c7aa670bc0e31c46bdf9c0
VidyaSagarMehar <vidyasagark890@gmail.com>
2024-02-10 00:35:07 +0530

    test for Header component added

---
+++ src/components/__tests__/Header.test.js
@@ -0,0 +1,45 @@
+import { render, screen } from '@testing-library/react';
+import Header from '../Header';
+import { Provider } from 'react-redux';
+import appStore from '../../utils/appStore';
+import { BrowserRouter } from 'react-router-dom';
+import '@testing-library/jest-dom';
+
+it('Should load Header component with a login button', () => {
+	//since we are using redux in our header so we have to wrap it inside the provider, just like we have wraped our whole app inside the provider
+	// we have to wrap it again inside the Browser router, Or Link will not work and gonna get error in Link element
+	render(
+		<BrowserRouter>
+			<Provider store={appStore}>
+				<Header />
+			</Provider>
+		</BrowserRouter>,
+	);
+
+	// const loginButton = screen.getByRole('button');
+
+	//Anothoer way to find login button
+	// const loginButton = screen.getByText('Login');
+
+	// another way to find specifially the Login button
+	const loginButton = screen.getByRole('button', { name: 'Login' });
+
+	expect(loginButton).toBeInTheDocument();
+});
+
+it('Should load Header component with a Cart item 0', () => {
+	render(
+		<BrowserRouter>
+			<Provider store={appStore}>
+				<Header />
+			</Provider>
+		</BrowserRouter>,
+	);
+
+	// const cartItems = screen.getByText('Cart (0 items)');
+
+	// By using REgex, so that we do not have to match the exact string
+	const cartItems = screen.getByText(/Cart/);
+
+	expect(cartItems).toBeInTheDocument();
+});
